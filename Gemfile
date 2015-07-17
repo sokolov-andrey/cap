cabe8ca68514fe21fe2ab7256583eb99345db463
Andrey Sokolov <asokolov@thumbtack.net>
2015-07-17 21:15:48 +0600

    add initial capistrano setup

--- Gemfile
+++ Gemfile
@@ -43,3 +43,9 @@ group :development, :test do
   gem 'spring'
 end
 
+group :development do
+  gem 'capistrano', '~> 3.4.0'
+  gem 'capistrano-rbenv', '~> 2.0'
+  gem 'capistrano-rails', '~> 1.1'
+  gem 'capistrano-bundler', '~> 1.1.2'
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,28 @@
+# server '192.168.33.11', user: 'vagrant', roles: %w{app}
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+
+# set :ssh_options, {
+#   keys: %w(/path_to/private_key),
+#   forward_agent: true
+# }
+
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
